4d84df895766989ea486a8571b29fee1addd625a
Thomas Bouve <crahan@n00.be>
2023-09-09 22:00:52 +0200

    Enable dock icon count again + new icon.

--- create_inoreader.sh
+++ create_inoreader.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 # Create Inoreader Electron app using a custom CSS and icon file
-nativefier 'https://www.inoreader.com/' Inoreader.app -n Inoreader --conceal -p mac --min-width 1200 --min-height 800 --darwin-dark-mode-support -i inoreader.png --title-bar-style hiddenInset --inject inoreader.css
+nativefier 'https://www.inoreader.com/' Inoreader.app -n Inoreader --counter --conceal -p mac --min-width 1200 --min-height 800 --darwin-dark-mode-support -i inoreader.png --title-bar-style hiddenInset --inject inoreader.css
 
 # Rename the output folder as it's using the same name as the app bundle
 mv Inoreader.app Inoreader_tmp
